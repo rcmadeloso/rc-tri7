e7e76e12b49e2d138a4b48f7123963486dfdf595
rcmadeloso <sr.rcmadeloso@gmail.com>
2023-02-21 18:54:58 +0800

    Modified ReadME

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -67,11 +67,34 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
                 </div>
 
                 <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                    Code that processes factorial using recursive faction written in javascript/vue
                 </p>
 
                 <p class="mt-4 text-sm">
                     <a target="_blank" href="/factorial" class="inline-flex items-center font-semibold text-indigo-700">
+                        Click here
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                            <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </p>
+            </div>
+
+
+            <div>
+                <div class="flex items-center">
+                    <h2 class="text-xl font-semibold text-gray-900">
+                        <a target="_blank" href="/iq-test">IQ Test</a>
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    Text File / Image answers to IQ Test
+                </p>
+
+                <p class="mt-4 text-sm">
+                    <a target="_blank" href="/iq-test" class="inline-flex items-center font-semibold text-indigo-700">
                         Click here
 
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
--- resources/js/Pages/Factorial/IQTest.vue
+++ resources/js/Pages/Factorial/IQTest.vue
@@ -1,3 +1,14 @@
 <template>
+
+	<div class="px-20 text-2xl">
+		
+	View Text File at : 
+
+	<a href="https://github.com/rcmadeloso/rc-tri7/blob/main/IQTestAnswers.txt" class="text-blue-500">
+		https://github.com/rcmadeloso/rc-tri7/blob/main/IQTestAnswers.txt
+	</a>
+
+	</div>
+
 	<img src="/images/IQTest.png">
 </template>
